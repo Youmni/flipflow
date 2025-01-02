eb5aa4b634ed5705f193c803d496662ff8507ea4
Youmni <youmni.malha@gmail.com>
2025-01-02 17:08:21 +0100

    Added UserController and configured routes for user operations

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,7 +25,7 @@ const createUser = async (req, res) => {
         const query = 'INSERT INTO users (first_name, last_name, username, email, password) VALUES (?, ?, ?, ?, ?)';
         const [results] = await connection.promise().query(query, [first_name, last_name, username, email, hashedPassword]);
 
-        res.status(201).json({ message: 'User created successfully', userId: results.insertId });
+        res.status(201).json({ message: 'User created successfully.', userId: results.insertId });
 
 
     }catch(err){
@@ -34,4 +34,67 @@ const createUser = async (req, res) => {
     }
 }
 
-export default createUser;
+const updateUser = async (req, res) => {
+
+    const { first_name, last_name, username, email} = req.body;
+    const userId = req.params.id;
+
+    try{
+        const [existingUser] = await connection.promise().query('SELECT * FROM users WHERE id = ?', [userId]);
+
+        if (existingUser.length === 0) {
+            return res.status(404).json({ message: 'User not found.' });
+        }
+
+        const query = 'UPDATE users SET first_name = ?, last_name = ?, username = ?, email = ? WHERE id = ?';
+        await connection.promise().query(query, [first_name, last_name, username, email, userId]);
+
+        res.status(200).json({ message: 'User updated successfully.' });
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ message: 'There was an error while updating the user.', error: err.message });
+    }
+}
+
+const updatePassword = async (req, res) => {
+    const { password } = req.body;
+    const userId = req.params.id;
+
+    try{
+        const [existingUser] = await connection.promise().query('SELECT * FROM users WHERE id = ?', [userId]);
+
+        if (existingUser.length === 0) {
+            return res.status(404).json({ message: 'User not found.' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const query = 'UPDATE users SET password = ?  WHERE id = ?';
+        await connection.promise().query(query, [hashedPassword, userId]);
+
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ message: 'There was an error while updating the password.', error: err.message });
+    }
+
+    res.status(200).json({ message: 'Password updated successfully.' });
+}
+
+const getUserById = async (req, res) => {
+    const userId = req.params.id;
+
+    try{
+        const [user] = await connection.promise().query('SELECT * FROM users WHERE id = ?', [userId]);
+
+        if (user.length === 0) {
+            return res.status(404).json({ message: 'User not found.' });
+        }
+
+        res.status(200).json({ user: user[0] });
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ message: 'There was an error fetching the user: ', error: err.message });
+    }
+}
+
+export { createUser, updateUser, updatePassword, getUserById };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import validateUser from '../middleware/validateUser.js';
-import createUser from '../controllers/userController.js';
+import {createUser,  getUserById,  updatePassword,  updateUser} from '../controllers/UserController.js';
 
 const router = express.Router();
 
 router.post("/register", validateUser, createUser);
+router.put("/update/:id", validateUser, updateUser);
+router.patch("/update/password/:id", validateUser, updatePassword);
+router.get("/:id", getUserById);
 
 export default router;
